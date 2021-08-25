4b57da1823d15af4703c96566282247ddf562269
edwardd15 <1872002@maranatha.ac.id>
2021-08-25 13:02:58 +0700

    17 Transaction Management dengan Spring JPA

---
+++ src/main/java/com/kelaskoding/dto/TransferRequest.java
@@ -0,0 +1,11 @@
+package com.kelaskoding.dto;
+
+import lombok.Data;
+
+@Data
+public class TransferRequest {
+    private String norek1;
+    private String norek2;
+    private double amount;
+    
+}
